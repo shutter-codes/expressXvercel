6a6c2464ab2f32e40147da14fc374d4f4d128f38
Naitik Sharma <snaitik0238@gmail.com>
2023-03-04 02:37:02 +0530

    Final commit

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ import express from "express";
 const app = express();
 const port = 9000;
 
-app.use("/",(req,res)=>{
-    res.json({message:"hello naitik"})
+app.use("/", (req, res) => {
+    res.json({ 
+        Name: "Naitik Sharma",
+        email: "snaitik0238@gmail.com",
+        College: "Arya College of Engineering & IT",
+        Gender: "male"
+    
+     })
 })
 
-app.listen(9000,()=>{
+app.listen(9000, () => {
     console.log(`starting server at port ${port}`);
 })
